3e727d4565cde397ffd12208c185e091e9a6fd48
Joel Burget <joelburget@gmail.com>
2022-04-02 07:22:32 -0700

    Resolution for webpack 5.
    I tested this by loading the index, confirming there are no errors and "Hello World" prints in the console.

--- js/next.config.js
+++ js/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    if (!config.resolve) {
+      config.resolve = {}
+    }
+    if (!config.resolve.fallback) {
+      config.resolve.fallback = {}
+    }
+    config.resolve.fallback.fs = false
+    config.resolve.fallback.child_process = false
+    return config
+  },
 }
 
 module.exports = nextConfig
